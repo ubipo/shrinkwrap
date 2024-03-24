5a307cfc0edfe9d20b138721fe1e0f4a9472d9d8
Pieter Fiers <pieter@pfiers.net>
2024-03-24 17:34:59 +1300

    :sparkles: Allow loading at runtime

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.UsesKotlinJavaToolchain
 import org.openstreetmap.josm.gradle.plugin.config.JosmPluginExtension
 import java.net.URI
-import java.net.URL
 
 val jvmTarget = 11
 val kotlinStdlibDependency = "stdlib-jdk8"
@@ -11,8 +9,6 @@ plugins {
     id("org.openstreetmap.josm").version("0.8.2")
 }
 
-//archivesBaseName = "Shrinkwrap"
-
 repositories {
     mavenCentral()
 }
@@ -21,14 +17,6 @@ dependencies {
     val kotlinStdlibDependencyId = kotlin(kotlinStdlibDependency)
     implementation(kotlinStdlibDependencyId)
     packIntoJar(kotlinStdlibDependencyId)
-}
-
-val service = project.extensions.getByType<JavaToolchainService>()
-val customLauncher = service.launcherFor {
-    languageVersion.set(JavaLanguageVersion.of(jvmTarget))
-}
-project.tasks.withType<UsesKotlinJavaToolchain>().configureEach {
-    kotlinJavaToolchain.toolchain.use(customLauncher)
 }
 
 kotlin {
